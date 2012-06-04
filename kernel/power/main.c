319fc70dbc9f04821d20884f21f876ff27c752da
Amar Singhal <asinghal@codeaurora.org>
2012-06-04 14:29:39 -0700

    power: main: Add conditional compilation for touch nodes
    Add conditional compilation for touch event sysfs nodes. Otherwise,
    if CONFIG_PM_SLEEP is not defined, there could be compilation errors.
    
    Change-Id: I1ac7f284ec35eae2cfa076ef8e71c29ddc24817c
    Signed-off-by: Amar Singhal <asinghal@codeaurora.org>
    Signed-off-by: Anurag Singh <anursing@codeaurora.org>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -608,8 +608,6 @@ power_attr(wake_unlock);
 #endif
 
 static struct attribute *g[] = {
-	&touch_event_attr.attr,
-	&touch_event_timer_attr.attr,
 	&state_attr.attr,
 #ifdef CONFIG_PM_TRACE
 	&pm_trace_attr.attr,
@@ -625,6 +623,8 @@ static struct attribute *g[] = {
 	&wake_lock_attr.attr,
 	&wake_unlock_attr.attr,
 #endif
+	&touch_event_attr.attr,
+	&touch_event_timer_attr.attr,
 #ifdef CONFIG_PM_DEBUG
 	&pm_test_attr.attr,
 #endif
