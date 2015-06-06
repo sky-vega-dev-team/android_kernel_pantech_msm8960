1e6a36b6039b00b228dcec4e273be14d8b331837
Lê Hoàng <icy.lord.love.angel@gmail.com>
2015-06-07 00:27:37 +0700

    Return to use Sabermod GCC
    This reverts commit b38b292a219964313634e4d0e7811f90e8dbebba.

--- ef52.sh
+++ ef52.sh
@@ -15,8 +15,8 @@
 export ARCH=arm
 #Patch to toolchain
 #export PATH=$(pwd)/../../../../arm-eabi-4.6/bin:$PATH
-export CROSS_COMPILE=~/android/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
-#export CROSS_COMPILE=~/android/mokee/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+#export CROSS_COMPILE=~/android/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=~/android/arm-linux-androideabi-4.9_sabermod/bin/arm-linux-androideabi-
 #Export Host name and user build
 export KBUILD_BUILD_USER=hl
 export KBUILD_BUILD_HOST=HL-G480
