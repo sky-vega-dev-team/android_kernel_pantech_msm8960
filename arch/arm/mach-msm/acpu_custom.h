09f243987427cba1dbd073d673a90dfcecc503b0
Lê Hoàng <icy.lord.love.angel@gmail.com>
2015-06-07 01:46:26 +0700

    Fix OC tables because random reboot

--- arch/arm/mach-msm/acpu_custom.h
+++ arch/arm/mach-msm/acpu_custom.h
@@ -14,7 +14,7 @@
 //Define for acpuclock-8064.c, acpuclock-krait.c, board-8064-regulator.c, msm_dcvs.c
 #if defined(CONFIG_CPU_OVERCLOCK) || defined(CONFIG_LOW_CPUCLOCKS)
 #define HFPLL_MIN_VDD		800000//Stock is 850000	/* uV */
-#define HFPLL_MAX_VDD		1350000//stock is 1300000	/* uV */
+#define HFPLL_MAX_VDD		1400000//stock is 1300000	/* uV */
 #else
 #define HFPLL_MIN_VDD		850000
 #define HFPLL_MAX_VDD		1300000
@@ -22,14 +22,14 @@
 
 //Loạn não :3
 #ifdef CONFIG_CPU_OVERCLOCK
-#define NUM_FREQS 10
+#define NUM_FREQS 5
 #else
 #define NUM_FREQS 0
 #endif
 
 #ifdef CONFIG_LOW_CPUCLOCKS
-#define FREQ_TABLE_SIZE		(36 + NUM_FREQS)
-#define DCVS_MAX_NUM_FREQS	(16 + NUM_FREQS)
+#define FREQ_TABLE_SIZE		(39 + NUM_FREQS)
+#define DCVS_MAX_NUM_FREQS	(19 + NUM_FREQS)
 #else
 #define DCVS_MAX_NUM_FREQS	(15 + NUM_FREQS)
 #define FREQ_TABLE_SIZE		(35 + NUM_FREQS)
