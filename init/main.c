c6529510888a3b0ebbe07ee869753e431baa29e7
Lê Hoàng <icy.lord.love.angel@gmail.com>
2015-11-10 21:58:56 +0700

    Merge tag 'v3.4.50' into LA.AF.1.1.1_kernel
    This is the 3.4.50 stable release
    
    Conflicts:
    	arch/arm/Kconfig
    	arch/arm/include/asm/mutex.h
    	arch/arm/kernel/perf_event.c
    	arch/arm/kernel/traps.c
    	arch/arm/mm/dma-mapping.c
    	arch/arm/mm/tlb-v7.S
    	arch/arm/vfp/vfpmodule.c
    	drivers/base/power/main.c
    	drivers/bluetooth/ath3k.c
    	drivers/bluetooth/btusb.c
    	drivers/gpu/drm/radeon/radeon_mode.h
    	drivers/mmc/card/block.c
    	drivers/mmc/host/sdhci.c
    	drivers/usb/core/message.c
    	fs/ubifs/dir.c
    	include/linux/kref.h
    	include/net/bluetooth/hci_core.h
    	include/net/bluetooth/mgmt.h
    	include/net/tcp.h
    	init/main.c
    	kernel/cgroup.c
    	kernel/events/core.c
    	kernel/signal.c
    	net/bluetooth/hci_conn.c
    	net/bluetooth/hci_core.c
    	net/bluetooth/hci_event.c
    	net/bluetooth/l2cap_core.c
    	net/bluetooth/mgmt.c
    	net/bluetooth/rfcomm/sock.c
    	net/bluetooth/smp.c
    	net/ipv4/ping.c
    	net/ipv4/syncookies.c
    	net/ipv6/route.c
    	net/ipv6/tcp_ipv6.c
    	net/wireless/reg.c
    
    Change-Id: Ic16d381fedbd35c45dc6ae120a8f2011876d7518
    Signed-off-by: Binoy Jayan <bjayan@codeaurora.org>
    
    Conflicts:
    	fs/ext4/Kconfig
    	init/main.c

--- init/main.c
+++ init/main.c
@@ -616,7 +616,7 @@ asmlinkage void __init start_kernel(void)
 	pidmap_init();
 	anon_vma_init();
 #ifdef CONFIG_X86
-	if (efi_enabled)
+	if (efi_enabled(EFI_RUNTIME_SERVICES))
 		efi_enter_virtual_mode();
 #endif
 	thread_info_cache_init();
@@ -643,6 +643,12 @@ asmlinkage void __init start_kernel(void)
 
 	acpi_early_init(); /* before LAPIC and SMP init */
 	sfi_init_late();
+
+#ifdef CONFIG_BOOT_TIME_MARKER
+	init_marker_proc_fs();
+#endif
+	if (efi_enabled(EFI_RUNTIME_SERVICES))
+		efi_free_boot_services();
 
 	ftrace_init();
 
